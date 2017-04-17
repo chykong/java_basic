ba5b99ecea1ea536010ed1ca0c245c81692e713c
chykong <chykong@163.com>
2017-04-17 23:03:24 +0800

    LogBack操作

---
+++ chapter1_basic/src/main/java/LogBackTest.java
@@ -0,0 +1,14 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by 孔垂云 on 2017/4/17.
+ * Logback测试
+ */
+public class LogBackTest {
+    private final static Logger logger = LoggerFactory.getLogger(LogBackTest.class);
+    public static void main(String[] args) {
+        logger.info("输出Info");
+        logger.error("输出Error");
+    }
+}
