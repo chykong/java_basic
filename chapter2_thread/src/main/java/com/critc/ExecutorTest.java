f0cc67eada71ddff6560de2ee269e9c8b6c3db5d
chykong <chykong@163.com>
2017-04-21 13:30:27 +0800

    线程池修改等待时间

--- chapter2_thread/src/main/java/com/critc/ExecutorTest.java
+++ chapter2_thread/src/main/java/com/critc/ExecutorTest.java
@@ -3,12 +3,13 @@ package com.critc;
 import java.util.Random;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by 孔垂云 on 2017/4/21.
  */
 public class ExecutorTest {
-    private static Executor executor = Executors.newFixedThreadPool(10);//新建一个10个线程的执行
+    private static Executor executor = Executors.newFixedThreadPool(10);//新建一个10个线程的线程池
 
     /**
      * 内部类实现一个线程
@@ -18,7 +19,7 @@ public class ExecutorTest {
         public void run() {
             System.out.println(Thread.currentThread().getName());//打印当前线程的名称
             try {
-                Thread.sleep(new Random().nextInt(10) * 1000);//暂定几秒钟
+                TimeUnit.SECONDS.sleep(new Random().nextInt(10));//暂停几秒钟
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
